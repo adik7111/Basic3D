1f6496717d35e8acc1e130fb562e8e5bf1387187
adik7111 <adrian.kucharski7111@hotmail.com>
2024-05-12 23:14:29 +0100

    Add Lighting and Camera Control

--- core/src/com/mygdx/basic3d/Basic3D.java
+++ core/src/com/mygdx/basic3d/Basic3D.java
@@ -6,16 +6,21 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.VertexAttributes;
+import com.badlogic.gdx.graphics.g3d.Environment;
 import com.badlogic.gdx.graphics.g3d.Material;
 import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 
 public class Basic3D implements ApplicationListener {
 
+	public Environment environment;
 	public PerspectiveCamera cam;
+	public CameraInputController camController;
 	public ModelBatch modelBatch;
 	public Model model;
 	public ModelInstance instance;
@@ -23,6 +28,9 @@ public class Basic3D implements ApplicationListener {
 	
 	@Override
 	public void create () {
+		environment = new Environment();
+		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+		environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
 		modelBatch = new ModelBatch();
 		cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		cam.position.set(10f, 10f, 10f);
@@ -36,6 +44,9 @@ public class Basic3D implements ApplicationListener {
 				new Material(ColorAttribute.createDiffuse(Color.GREEN)),
 				VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
 		instance = new ModelInstance(model);
+
+		camController = new CameraInputController(cam);
+		Gdx.input.setInputProcessor(camController);
 	}
 
 	@Override
@@ -45,11 +56,12 @@ public class Basic3D implements ApplicationListener {
 
 	@Override
 	public void render () {
+		camController.update();
 		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
 		modelBatch.begin(cam);
-		modelBatch.render(instance);
+		modelBatch.render(instance, environment);
 		modelBatch.end();
 	}
 
