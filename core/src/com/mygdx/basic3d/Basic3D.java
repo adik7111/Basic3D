182d86faa536774471c689f101351f4e79623009
adik7111 <adrian.kucharski7111@hotmail.com>
2024-05-12 23:45:07 +0100

    Add multiple rendering of ship

--- core/src/com/mygdx/basic3d/Basic3D.java
+++ core/src/com/mygdx/basic3d/Basic3D.java
@@ -2,19 +2,19 @@ package com.mygdx.basic3d;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.assets.loaders.ModelLoader;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
-import com.badlogic.gdx.graphics.VertexAttributes;
 import com.badlogic.gdx.graphics.g3d.Environment;
-import com.badlogic.gdx.graphics.g3d.Material;
 import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.loader.ObjLoader;
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
-import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.utils.Array;
 
 public class Basic3D implements ApplicationListener {
 
@@ -22,31 +22,45 @@ public class Basic3D implements ApplicationListener {
 	public PerspectiveCamera cam;
 	public CameraInputController camController;
 	public ModelBatch modelBatch;
-	public Model model;
-	public ModelInstance instance;
+	public AssetManager assets;
+	public Array<ModelInstance> instances = new Array();
+	public boolean loading;
 
 	
 	@Override
 	public void create () {
+		modelBatch = new ModelBatch();
 		environment = new Environment();
 		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
 		environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
-		modelBatch = new ModelBatch();
+
 		cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		cam.position.set(10f, 10f, 10f);
+		cam.position.set(7f, 7f, 7f);
 		cam.lookAt(0,0,0);
 		cam.near = 1f;
 		cam.far = 300f;
 		cam.update();
 
-		ModelBuilder modelBuilder = new ModelBuilder();
-		model = modelBuilder.createBox(5f, 5f, 5f,
-				new Material(ColorAttribute.createDiffuse(Color.GREEN)),
-				VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
-		instance = new ModelInstance(model);
-
 		camController = new CameraInputController(cam);
 		Gdx.input.setInputProcessor(camController);
+
+		assets = new AssetManager();
+		assets.load("ship.obj", Model.class);
+		loading = true;
+	}
+
+	private void doneLoading() {
+		Model ship = assets.get("ship.obj", Model.class);
+		for (float x = -5f; x <= 5f; x += 2f) {
+			for (float y = -2f; y <= 2f; y += 2f) {
+				for (float z = -5f; z <= 5f; z += 2f) {
+					ModelInstance shipInstance = new ModelInstance(ship);
+					shipInstance.transform.setToTranslation(x, y, z);
+					instances.add(shipInstance);
+				}
+			}
+		}
+		loading = false;
 	}
 
 	@Override
@@ -56,12 +70,15 @@ public class Basic3D implements ApplicationListener {
 
 	@Override
 	public void render () {
+		if (loading && assets.update()) {
+			doneLoading();
+		}
 		camController.update();
 		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
 		modelBatch.begin(cam);
-		modelBatch.render(instance, environment);
+		modelBatch.render(instances, environment);
 		modelBatch.end();
 	}
 
@@ -78,6 +95,7 @@ public class Basic3D implements ApplicationListener {
 	@Override
 	public void dispose () {
 		modelBatch.dispose();
-		model.dispose();
+		instances.clear();
+		assets.dispose();
 	}
 }
